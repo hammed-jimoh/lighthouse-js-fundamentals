e41769c270b17336f34c8781c13887a16bb51a5a
hammed-jimoh <hammedgjimoh@yahoo.com>
2021-10-06 02:02:46 +0000

    Loopy Lighthouse Quiz

---
+++ loopy-lighthouse1.js
@@ -0,0 +1,16 @@
+// LoopyLighthouse quiz
+let n = 0;
+for (let n = 100; n <= 200; n ++) {
+  if (n % 3 === 0 && n % 4 === 0) {
+    console.log ("LoopyLighthouse");
+  } 
+  else if (n % 4 === 0) {
+    console.log ("Lighthouse");
+  }
+  else if (n % 3 === 0) {
+    console.log ("Loopy");
+  }
+  else {
+    console.log (n);
+  }
+}
