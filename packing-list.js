a99f2d4bda9caaf56b1bab59f73de48d4f1eeae7
hammed-jimoh <hammedgjimoh@yahoo.com>
2021-10-19 23:02:02 +0000

    Packing list exercise using for-loop and while-loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,15 @@
+// Using for-loop
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+//Using while-loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
