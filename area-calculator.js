45858fc8977e6fb98b0ffc8738cdf7c669f39659
hammed-jimoh <hammedgjimoh@yahoo.com>
2021-10-19 15:49:24 +0000

    Area calculator challenge

--- area-calculator.js
+++ area-calculator.js
@@ -1,22 +1,26 @@
 function calculateRectangleArea(length, width){
   if (length < 0 || width < 0) {
-    return undefined
+    return undefined;
   }
   const calculateRectangleArea = length * width;
   return calculateRectangleArea;
-};
+}
 console.log(calculateRectangleArea(1.50, 2.5));
 
 function calculateCircleArea(radius){
+  if (radius < 0) {
+    return undefined;
+  }
   const calculateCircleArea = (Math.PI * Math.pow(radius, 2));
   return calculateCircleArea.toFixed(3);
-};
+}
 console.log(calculateCircleArea(3.5));
 
 function calculateTriangleArea(base, height){
+  if (height < 0 || base < 0) {
+    return undefined;
+  }
   const calculateTriangleArea = (base * height) / 2;
   return calculateTriangleArea;
-};
+}
 console.log(calculateTriangleArea(3, 2.5));
-
-
