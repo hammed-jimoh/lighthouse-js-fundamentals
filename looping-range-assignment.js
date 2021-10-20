7185250a3d4f8b00b967bcdda822d6a34bde489a
hammed-jimoh <hammedgjimoh@yahoo.com>
2021-10-20 01:45:37 +0000

    start, end, step loop function array

---
+++ looping-range-assignment.js
@@ -0,0 +1,13 @@
+const range = (start, end, step) => {
+  let newArray = []
+  for (let i = start; i <= end; i += step) {
+    if (start === undefined || end === undefined || step === undefined || start > end || step <= 0) {
+      newArray = []
+    }
+    newArray.push(i)
+  }
+  return newArray
+}
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
