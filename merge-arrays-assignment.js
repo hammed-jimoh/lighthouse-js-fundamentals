d8229fe35f7749f5a1493a120fff72faf72a5e64
hammed-jimoh <hammedgjimoh@yahoo.com>
2021-10-24 23:12:08 +0000

    edited merge array assignmet

--- merge-arrays-assignment.js
+++ merge-arrays-assignment.js
@@ -1,7 +1,19 @@
 const merge = (arr1, arr2) => {
-  let result = Array.prototype.concat(arr1, arr2);
-  return result.sort((a, b) => a - b);
+  for (let i = 0; i < arr2.length; i ++){
+    arr1.push(arr2[i]);
+  }
+  console.log(arr1)
+// rearrange a merged array into ascending order: the new merged array is arr1
+// k is the first element of the array and k+1 is the second element of the array
+  for (let j = 0; j < arr1.length; j++){
+    for (let k = 0; k < arr1.length - j - 1; k++) {
+      if(arr1[k + 1] < arr1[k]) {
+        [arr1[k], arr1[k+1]] = [arr1[k+1], arr1[k]]
+       }
+    }
+  }
+  return arr1
 }
+console.log(merge([4], [2, 5, 8]));
 console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]));
-console.log(merge([ 4 ], [ 2, 5, 8 ]));
 console.log(merge([ 1, 2, 6 ], []));
